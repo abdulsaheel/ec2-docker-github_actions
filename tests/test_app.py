af1971f0de678d448bb3d23f7db0971125eabc35
Mohammad Abdul Sahil || Python Programmer <127765312+abdulsaheel@users.noreply.github.com>
2024-08-04 16:08:23 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -14,4 +14,4 @@ def client():
 def test_homepage(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert response.data == b'Hello, World!'
+    assert response.data == b'Hello, World! Update text'
